07d1b1e9173a4f0a654728d7cb46d166111e0ce3
Kreoniz <kreoniz@mail.ru>
2024-01-26 13:14:14 +0600

    Display weather information on the page

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 const API_KEY = 'f9cb5c0856e8459792954125241701';
 const BASE_URL = 'https://api.weatherapi.com/v1';
 
+const mode = 'celsius';
+
+const modeSymbols = {
+  'celsius': '°C',
+  'fahrenheit': '°F',
+}
+
 const parseJson = function parseJsonResponseData(json) {
   const city = json.location.name;
   const country = json.location.country;
@@ -13,7 +20,7 @@ const parseJson = function parseJsonResponseData(json) {
     forecast[i - 1] = {
       date: day.date,
       condition: day.day.condition,
-      celcius: {
+      celsius: {
         min: day.day.mintemp_c,
         max: day.day.maxtemp_c,
         average: day.day.avgtemp_c,
@@ -28,9 +35,19 @@ const parseJson = function parseJsonResponseData(json) {
 
   return {
     location: { city, country },
-    current: {
-      celcius: { current: json.current.temp_c, feelslike: json.current.feelslike_c },
-      fahrenheit: { current: json.current.temp_f, feelslike: json.current.feelslike_f },
+    today: {
+      celsius: {
+        current: json.current.temp_c,
+        feelslike: json.current.feelslike_c,
+        min: forecastData[0].day.mintemp_c,
+        max: forecastData[0].day.maxtemp_c,
+      },
+      fahrenheit: {
+        current: json.current.temp_f,
+        feelslike: json.current.feelslike_f,
+        min: forecastData[0].day.mintemp_f,
+        max: forecastData[0].day.maxtemp_f,
+      },
       condition: json.current.condition,
     },
     forecast,
@@ -40,6 +57,7 @@ const parseJson = function parseJsonResponseData(json) {
 const getWeather = async function getWeatherData(city) {
   const response = await fetch(`${BASE_URL}/forecast.json?key=${API_KEY}&q=${city}&days=3`, { mode: 'cors' });
   const weatherData = await response.json();
+  console.log('Original:', weatherData);
 
   const parsedData = parseJson(weatherData);
 
@@ -58,15 +76,43 @@ const displayWeather = function displayWeather(city) {
       const input = document.querySelector('#input');
       input.value = `${data.location.city}, ${data.location.country}`;
 
-      const currentIcon = document.querySelector('#current-icon');
-      currentIcon.src = 'https:' + data.current.condition.icon.replaceAll('64', '128');
+      const todayIcon = document.querySelector('#today-icon');
+      const todayStatus = document.querySelector('#today-status');
+      todayIcon.src = 'https:' + data.today.condition.icon.replaceAll('64', '128');
+      todayStatus.textContent = data.today.condition.text;
+
+      const symbol = modeSymbols[mode];
+      const weatherToday = data.today[mode];
+
+      const todayInfo = document.querySelector('#today-info');
+      const todayCurrent = todayInfo.querySelector('.today-current');
+      const todayFeelsLike = todayInfo.querySelector('.today-feels-like');
+      const todayMin = todayInfo.querySelector('.today-min');
+      const todayMax = todayInfo.querySelector('.today-max');
+
+      todayCurrent.textContent = weatherToday.current + symbol;
+      todayFeelsLike.textContent = weatherToday.feelslike + symbol;
+      todayMin.textContent = weatherToday.min + symbol;
+      todayMax.textContent = weatherToday.max + symbol;
 
       const forecastBlock = document.querySelector('#forecasts');
       const forecasts = forecastBlock.querySelectorAll('.forecast');
-      console.log(forecasts);
 
       for (let i = 0; i < 2; i += 1) {
-        forecasts[i].querySelector('img').src = 'https:' + data.forecast[i].condition.icon;
+        const forecastImg = forecasts[i].querySelector('img');
+        const forecastStatus = forecasts[i].querySelector('.forecast-status');
+        const forecastMin = forecasts[i].querySelector('.forecast-min');
+        const forecastMax = forecasts[i].querySelector('.forecast-max');
+        const forecastAvg = forecasts[i].querySelector('.forecast-avg');
+
+        const weatherForecast = data.forecast[i];
+
+        forecastImg.src = 'https:' + weatherForecast.condition.icon;
+        forecastStatus.textContent = weatherForecast.condition.text;
+
+        forecastMin.textContent = weatherForecast[mode].min + symbol;
+        forecastMax.textContent = weatherForecast[mode].max + symbol;
+        forecastAvg.textContent = weatherForecast[mode].average + symbol;
       }
 
       return data;
